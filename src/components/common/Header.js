88a00b326277b687f88e187b696290121275ba0f
chaerin <achrvv@sookmyung.ac.kr>
2021-06-30 16:25:53 +0900

    feat: style theme 적용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router";
+import styled from "styled-components";
+
+const HeaderWrap = styled.div`
+  font: ${({ theme }) => theme.font.display1};
+  color: ${({ theme }) => theme.color.primary};
+`;
 
 const Header = () => {
   const location = useLocation();
@@ -8,7 +14,9 @@ const Header = () => {
     console.log(location);
   }, []);
 
-  return <>{location.pathname !== "/login" && <div>Header입니다</div>}</>;
+  return (
+    <>{location.pathname !== "/login" && <HeaderWrap>Header</HeaderWrap>}</>
+  );
 };
 
 export default Header;
---
+++ src/styles/theme.js
@@ -0,0 +1,40 @@
+//how to use
+//font: ${({ theme }) => theme.font.display1};
+// color: ${({ theme }) => theme.color.primary};
+
+const color = {
+  primary: "#FDCB02",
+  primary_light: "#FFE371",
+  primary_lighter: "#FFEEBA",
+  primary_dark: "#E7BB0B",
+  primary_darker: "#D19701",
+  white: "#FFFFFF",
+  lightgray1: "#F8F6F6",
+  lightgray2: "#DFDFDF",
+  gray1: "#C1C1C1",
+  gray2: "#A5A5A5",
+  gray3: "#6F6F6F",
+  darkgray1: "#3D3D3D",
+  darkgray2: "#1A1A1A",
+  black: "#000000",
+  bg_yellow: "#FFFCF1",
+  bg_gray: "#F8F8F8",
+  bg_sky: "#F0F8FE"
+};
+
+const font = {
+  display1: "normal 700 36px/49px 'Noto Sans kr'",
+  headline: "normal 500 24px/35px 'Noto Sans kr'",
+  title: "normal 700 22px/30px 'Noto Sans kr'",
+  subheading: "normal 500 18px/26px 'Noto Sans kr'",
+  body2: "normal 400 16px/23px 'Noto Sans kr'",
+  body1: "normal 400 14px/20px 'Noto Sans kr'",
+  caption: "normal 400 12px/17px 'Noto Sans kr'",
+  button: "normal 500 18px/22px 'Spoqa Han Sans Neo'"
+};
+const theme = {
+  color,
+  font
+};
+
+export default theme;
