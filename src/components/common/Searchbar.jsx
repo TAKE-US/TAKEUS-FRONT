dc45f7b09f2e99b4c5c42bff0ee666be315151cc
moonho park <moonho4199@naver.com>
2021-07-02 20:22:46 +0900

    feat: dropdown component

---
+++ src/components/common/Searchbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Dropdown from './Dropdown';
+
+const Searchbar = () => {
+  return (
+    <>
+      <Dropdown />
+    </>
+  );
+};
+
+export default Searchbar;
