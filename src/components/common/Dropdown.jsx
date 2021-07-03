8fd837598ee05bcdff1144d1c278a6a45d302615
moonho park <moonho4199@naver.com>
2021-07-03 23:22:21 +0900

    feat: dropdown 외부 클릭시 비활성화

--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
+import { useDetectOutsideClick } from '../../hooks/useDetectOutsideClick';
 import arrow_bottom from '../../assets/img/ic_arrow_bottom_black_24.svg';
 
 const MenuContainer = styled.div`
@@ -8,8 +9,8 @@ const MenuContainer = styled.div`
 `;
 
 const MenuButton = styled.button`
-  width: 14rem;
-  height: 7.4rem;
+  width: 21rem;
+  height: 8.2rem;
   font: ${({ theme }) => theme.font.subheading};
   display: flex;
   justify-content: space-between;
@@ -25,6 +26,10 @@ const MenuButton = styled.button`
 const MenuNav = styled.nav`
   position: absolute;
   display: ${props => props.isActive ? 'flex' : 'none'};
+  width: 19.8rem;
+  padding: 1rem 0.6rem;
+  box-shadow: 0rem 0rem 3rem 0.1rem rgba(0,0,0,0.1);
+  border-radius: 1rem;
 `;
 
 const MenuUl = styled.ul`
@@ -34,23 +39,27 @@ const MenuUl = styled.ul`
 `;
 
 const MenuList = styled.li`
-  width: 14rem;
-  height: 7.4rem;
+  width: 19.8rem;
+  height: 3.4rem;
   font: ${({ theme }) => theme.font.subheading};
   display: flex;
   justify-content: center;
   align-items: center;
+  &:hover {
+    cursor: pointer;
+    background-color: ${({ theme }) => theme.color.bg_yellow};
+  }
 `;
 
 const Dropdown = () => {
   const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
   const onClick = () => {
     setIsActive(!isActive);
     console.log(isActive);
   };
 
-  const country = ["미국", "프랑스", "독일", "이탈리아", "스위스"];
+  const country = ["미국", "캐나다", "독일"];
 
   return (
     <MenuContainer>
