4fc50e986c4981719452464bb40ff4328ff53b03
leeinsong <insong1216@gmail.com>
2021-07-04 23:10:29 +0900

    feat: review comment 반영

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,4 @@
 export { default as Header } from "./common/Header";
 export { default as FindDogCard } from "./atoms/FindDogCard";
+export { default as CarouselDogContainer } from "./molecules/CarouselDogContainer";
+export { default as CarouselReviewContainer } from "./molecules/CarouselReviewContainer";
