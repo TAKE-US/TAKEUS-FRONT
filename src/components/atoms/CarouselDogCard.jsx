83d58e77bbb7a2d225f59782eae702f2e13c059e
leeinsong <insong1216@gmail.com>
2021-07-04 14:56:55 +0900

    add: carousel dog container & card 추가

---
+++ src/components/atoms/CarouselDogCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const CarouselDogCard = () => {
+  return <div></div>;
+};
+
+export default CarouselDogCard;
---
+++ src/components/molecules/CarouselDogContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import CarouselDogCard from "../atoms/CarouselDogCard";
+
+const CarouselDogContainer = () => {
+  return <div></div>;
+};
+
+export default CarouselDogContainer;
