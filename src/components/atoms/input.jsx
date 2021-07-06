9610765bbe50046c02a6dee066664d1be5f66bea
JunyupHong <junyup.hong@gmail.com>
2021-07-07 01:31:32 +0900

    feat: input component 마크업

---
+++ src/components/atoms/input.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useMemo, useState } from "react";
+import styled from "styled-components";
+
+const Styled = {
+  InputWrapper: styled.div`
+    display: flex;
+    width: 100%;
+    padding: 4px 20px;
+    padding-left: 20px;
+    border: solid 1px ${({ theme }) => theme.color.lightgray2};
+    border-radius: 54px;
+  `,
+  Input: styled.input`
+    flex: 1;
+    font: ${({ theme }) => theme.font.button};
+    color: ${({ theme }) => theme.color.darkgray1};
+    line-height: 23px;
+    border: none;
+
+    &::placeholder {
+      line-height: 26px;
+      color: ${({ theme }) => theme.color.gray1};
+    }
+  `,
+  Desc: styled.p`
+    margin-left: 6px;
+    font-size: 12px;
+    line-height: 17px;
+    color: ${({ theme }) => theme.color.gray3};
+    &.error {
+      color: #F2754E;
+    }
+  `,
+  Child: styled.div``,
+  Divider: styled.div`
+    margin: 0 16px;
+    width: 0;
+    border-right: solid 1px ${({ theme }) => theme.color.lightgray2};
+  `
+};
+
+const Input = ({ children, placeholder, description, max, childPos }) => {
+  const [word, setWord] = useState("");
+  const [isError, setError] = useState(false);
+
+  useEffect(() => {
+    if (!max) return;
+
+    if (word.length > max) setError(true);
+    else setError(false);
+  }, [word, max]);
+
+  const hasLeftChild = useMemo(() => children && childPos !== 'right' ? true : false, [children, childPos]);
+
+  const changeWord = evt => {
+    setWord(evt.target.value);
+  };
+
+  return (
+    <>
+      <Styled.InputWrapper hasLeftChild={hasLeftChild}
+      >
+        {
+          children && childPos !== 'right' ?
+            (<>
+              <Styled.Child className="test">{children}</Styled.Child>
+              <Styled.Divider></Styled.Divider>
+            </>) : ''
+        }
+        <Styled.Input
+          placeholder={placeholder}
+          value={word}
+          onChange={changeWord}
+        />
+        {
+          children && childPos === 'right' ?
+            (<>
+              <Styled.Child className="test">{children}</Styled.Child>
+              <Styled.Divider></Styled.Divider>
+            </>) : ''
+        }
+      </Styled.InputWrapper>
+      <Styled.Desc className={isError ? "error" : ""}>{description}</Styled.Desc>
+    </>
+  );
+};
+
+export default Input;
--- src/pages/dogEnroll/DogEnrollPage.jsx
+++ src/pages/dogEnroll/DogEnrollPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Input from 'components/atoms/input';
+
 const DogEnrollPage = () => {
-  return <div>대상견 등록</div>;
+  return (
+    <>
+      <Input placeholder={'test'} description={'최대 10 글자'} max={10} childPos={'left'}>
+        <div>dropdown slot</div>
+      </Input>
+    </>
+  );
 };
 
 export default DogEnrollPage;
