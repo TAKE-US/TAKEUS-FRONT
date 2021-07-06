08998e6d6e80a25c55350b8e77b3a7a005a239c4
chaerin <achrvv@sookmyung.ac.kr>
2021-07-06 17:01:18 +0900

    feat: filter 퍼블리싱

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,16 +4,17 @@ export { default as DogDetailInfo } from "./atoms/DogDetailInfo";
 export { default as DogDetail } from "./atoms/DogDetail";
 export { default as FindDogCard } from "./atoms/FindDogCard";
 export { default as Searchbar } from "./atoms/Searchbar";
+export { default as DogFilter } from "./atoms/DogFilter";
 
 // layer
 export { default as Header } from "./layer/Header";
-export { default as Footer } from './layer/Footer';
+export { default as Footer } from "./layer/Footer";
 
 // layer/mainPage
-export { default as AboutTakeUs } from './layer/mainPage/AboutTakeUs';
-export { default as BeforeVolunteer } from './layer/mainPage/BeforeVolunteer';
-export { default as CarouselDogContainer } from './layer/mainPage/CarouselDogContainer';
-export { default as CarouselReviewContainer } from './layer/mainPage/CarouselReviewContainer';
+export { default as AboutTakeUs } from "./layer/mainPage/AboutTakeUs";
+export { default as BeforeVolunteer } from "./layer/mainPage/BeforeVolunteer";
+export { default as CarouselDogContainer } from "./layer/mainPage/CarouselDogContainer";
+export { default as CarouselReviewContainer } from "./layer/mainPage/CarouselReviewContainer";
 
 // layer/about
 
