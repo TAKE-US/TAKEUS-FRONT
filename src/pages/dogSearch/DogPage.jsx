fb1bf5f5d78631c22e3d46a048495a94aba16909
chaerin <achrvv@sookmyung.ac.kr>
2021-07-07 23:23:24 +0900

    fix: conflict 해결

--- src/pages/dogSearch/DogPage.jsx
+++ src/pages/dogSearch/DogPage.jsx
@@ -1,18 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { DogCardContainer, PaginationNav, DogFilter, DogSearchNavigation } from "../../components";
-import styled from "styled-components";
 //api
 import { getDogs } from "lib/api/sample";
-
-const DogPageWrap = styled.section`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  .container-div {
-    padding-bottom: 9.7rem;
-    border-bottom: 1px solid ${({ theme }) => theme.color.lightgray2};
-  }
-`;
 
 const DogPage = () => {
   const [dogs, setDogs] = useState([]);
@@ -27,7 +16,7 @@ const DogPage = () => {
 
   return (
     <>
-    <DogSearchNavigation />
+      <DogSearchNavigation />
       <DogFilter />
       <div className="container-div">
         <DogCardContainer dogs={dogs} />
