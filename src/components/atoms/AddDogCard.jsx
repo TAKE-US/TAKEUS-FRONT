aaceefbe077cb2673ee777df375fc9a1df8911d4
leeinsong <insong1216@gmail.com>
2021-07-07 20:18:01 +0900

    style: card 추가 기능 스타일 추가

--- src/components/atoms/AddDogCard.jsx
+++ src/components/atoms/AddDogCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-// import plus from "assets/img/plus_icon.svg";
+import plus from "assets/img/ic_plus.svg";
 
 const Styled = {
   Wrapper: styled.section`
@@ -11,21 +11,35 @@ const Styled = {
     border-radius: 1rem;
     background-color: #f8f8f8;
     margin-right: 1.5rem;
-    img {
+    .card {
+      position: relative;
+      &__img {
+        position: absolute;
+        left: 3.8rem;
+        max-width: 2rem;
+        max-height: 2rem;
+      }
+      &__content {
+        position: absolute;
+        width: 12rem;
+        left: 6.2rem;
+        font: ${({ theme }) => theme.font.button};
+        color: ${({ theme }) => theme.color.primary};
+      }
+      &__input {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        :hover {
+          cursor: pointer;
+        }
+      }
+    }
+    .image_area > img {
       width: 20rem;
       height: 20rem;
       border-radius: 1rem;
-    }
-    p {
-      z-index: 0;
-    }
-    input {
-      width: 20rem;
-      height: 20rem;
-      opacity: 0;
-    }
-    input:hover {
-      cursor: pointer;
     }
   `,
 };
@@ -45,7 +59,10 @@ const AddDogCard = ({ count, setCount }) => {
     <Styled.Wrapper>
       {!imgfile ? (
         <div className="card">
+          <img className="card__img" src={plus} alt="plus" />
+          <p className="card__content">사진 추가하기</p>
           <input
+            className="card__input"
             type="file"
             id="detail_image"
             accept="image/*"
@@ -73,8 +90,3 @@ const AddDogCard = ({ count, setCount }) => {
 };
 
 export default AddDogCard;
-
-{
-  /* <img src={plus} alt="plus" />
-            <p>사진 추가하기</p> */
-}
