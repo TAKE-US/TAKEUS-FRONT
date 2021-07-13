10698fc080a726350975ac3eeea5dcc57874dd09
jenehojinchoi <jenehojinchoi@gmail.com>
2021-07-13 15:22:12 +0900

    style: LoginLayer logo, responsive web

--- src/components/layer/login/LoginLayer.jsx
+++ src/components/layer/login/LoginLayer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import LoginImg from "../../../assets/img/img_Login.png";
-import KakaotalkIcon from "../../../assets/img/ic_kakaotalk.svg";
-import NaverIcon from "../../../assets/img/ic_naver.svg";
-import GoogleIcon from "../../../assets/img/ic_google.svg";
+import LoginImg from "assets/img/img_Login.png";
+import KakaotalkIcon from "assets/img/ic_kakaotalk.svg";
+import NaverIcon from "assets/img/ic_naver.svg";
+import GoogleIcon from "assets/img/ic_google.svg";
+import { ReactComponent as Title } from "assets/icon/ic_logo_wordmark_middle.svg";
+import { ReactComponent as SubTitle } from "assets/icon/Group.svg";
 import { GoogleLogin } from "react-google-login";
 
 const Styled = {
@@ -12,36 +14,58 @@ const Styled = {
     flex-direction: space-between;
     position: absolute;
     left: 0rem;
-    width: 144rem;
-    height: 102.4rem;
+    width: 100vw;
+    height: 132%;
+
+    div {
+      width: 45.347%;
+      background-img: cover;
+    }
   `,
 
   Image: styled.img`
-    height: 100%;
+    position: absolute;
+    width: 45.37%;
+  `,
+
+  Title: styled.div`
+    position: absolute;
+    top: 50%;
+    left: 16%;
+    display: flex;
+    flex-direction: column;
+    z-index: 1;
   `,
 
   Section: styled.section`
+    width: 54.652%;
+    height: 100%;
     display: flex;
-    flex-direction: column;
-    margin-left: 16.7rem;
-    padding-top: 34.7rem;
+    justify-content: center;
+    align-items: center;
 
-    h1 {
-      height: 4.9rem;
-      margin-bottom: 0.7rem;
-      font: ${({ theme }) => theme.font.display2};
-      color: ${({ theme }) => theme.color.black};
-    }
+    div {
+      display: flex;
+      flex-direction: column;
 
-    h2 {
-      height: 2.2rem;
-      margin-bottom: 7rem;
-      font: ${({ theme }) => theme.font.body1};
-      color: ${({ theme }) => theme.color.gray3};
-    }
+      h1 {
+        height: 4.9rem;
+        margin-bottom: 0.7rem;
+        font: ${({ theme }) => theme.font.display2};
+        color: ${({ theme }) => theme.color.black};
+      }
+  
+      h2 {
+        height: 2.2rem;
+        margin-bottom: 7rem;
+        font: ${({ theme }) => theme.font.body1};
+        color: ${({ theme }) => theme.color.gray3};
+      }
+  
+      .google {
+        border: 0.1rem solid #dfdfdf;
+      }
 
-    .google {
-      border: 1px solid #dfdfdf;
     }
   `,
 
@@ -98,29 +122,37 @@ const LoginLayer = () => {
   };
   return (
     <Styled.Wrapper>
-      <Styled.Image src={LoginImg} alt="dogs" />
+      <div>
+        <Styled.Title>
+          <Title margin-bottom="2.1rem"/>
+          <SubTitle />
+        </Styled.Title>   
+        <Styled.Image src={LoginImg} alt="dogs" />
+      </div>
       <Styled.Section>
-        <h1>Takeus 시작하기</h1>
-        <h2>SNS 계정으로 손쉽게 가입하고 Takers가 될 수 있어요 :)</h2>
-        <Styled.Button type="button" color={"#FEE500"}>
-          <img className="kakaotalkIcon" src={KakaotalkIcon} alt="kakakotalk" />
-          카카오톡으로 시작하기
-        </Styled.Button>
-        <Styled.Button type="button" color={"#1EC800"}>
-          <img className="naverIcon" src={NaverIcon} alt="naver" />
-          네이버로 시작하기
-        </Styled.Button>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENTID}
-          render={renderProps => (
-            <Styled.Button className="google" type="button" color={"white"} onClick={renderProps.onClick}>
-              <img className="googleIcon" src={GoogleIcon} alt="google" />
-              구글로 시작하기
-            </Styled.Button>
-          )}
-          onSuccess={handleSuccess}
-          onFailure={handleFailure}
-        />
+        <div>
+          <h1>Takeus 시작하기</h1>
+          <h2>SNS 계정으로 손쉽게 가입하고 Takers가 될 수 있어요 :)</h2>
+          <Styled.Button type="button" color={"#FEE500"}>
+            <img className="kakaotalkIcon" src={KakaotalkIcon} alt="kakakotalk" />
+            카카오톡으로 시작하기
+          </Styled.Button>
+          <Styled.Button type="button" color={"#1EC800"}>
+            <img className="naverIcon" src={NaverIcon} alt="naver" />
+            네이버로 시작하기
+          </Styled.Button>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENTID}
+            render={renderProps => (
+              <Styled.Button className="google" type="button" color={"white"} onClick={renderProps.onClick}>
+                <img className="googleIcon" src={GoogleIcon} alt="google" />
+                구글로 시작하기
+              </Styled.Button>
+            )}
+            onSuccess={handleSuccess}
+            onFailure={handleFailure}
+            />
+          </div>
       </Styled.Section>
     </Styled.Wrapper>
   );
