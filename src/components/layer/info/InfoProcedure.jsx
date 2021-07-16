dbbc8e8fad82c4aa1b3868eb6931c7c5d3b7a058
moonho park <moonho4199@naver.com>
2021-07-16 19:38:00 +0900

    style: 이동봉사정보 line-height

--- src/components/layer/info/InfoProcedure.jsx
+++ src/components/layer/info/InfoProcedure.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { Button } from 'components';
-import BackgroundImg from 'assets/img/img_info_background-bottom.jpg';
-import Step1Img from 'assets/img/img_info_step1.png';
-import Step3Img from 'assets/img/img_info_step3.png';
-import Step4Img from 'assets/img/img_info_step4.png';
+import { Button } from "components";
+import BackgroundImg from "assets/img/img_info_background-bottom.jpg";
+import Step1Img from "assets/img/img_info_step1.png";
+import Step3Img from "assets/img/img_info_step3.png";
+import Step4Img from "assets/img/img_info_step4.png";
 
 const Styled = {
   Wrapper: styled.div`
@@ -15,7 +15,6 @@ const Styled = {
     background: url(${BackgroundImg});
     background-position: 100%;
     background-size: cover;
-    
 
     .content-wrapper {
       width: 1080px;
@@ -35,14 +34,14 @@ const Styled = {
         align-items: flex-end;
         margin-top: 4rem;
         margin-left: 7.6rem;
-        
+
         .card {
           display: inline-flex;
           margin: 4.2rem 0;
           padding: 4rem;
           border-radius: 10px;
           box-shadow: 0px 0px 15px 1px rgba(0, 0, 0, 0.07);
-          
+
           &__title {
             margin-bottom: 1.5rem;
             font: ${({ theme }) => theme.font.title2};
@@ -55,6 +54,7 @@ const Styled = {
             font: ${({ theme }) => theme.font.body3};
             color: ${({ theme }) => theme.color.darygray1};
             list-style-type: disc;
+            line-height: 2.4rem;
             &::marker {
               font-size: 8px;
             }
@@ -70,19 +70,20 @@ const Styled = {
           }
         }
 
-
         .last-title {
           width: 100%;
           margin-top: 4.1rem;
           margin-bottom: 3rem;
           font: ${({ theme }) => theme.font.headline};
           color: ${({ theme }) => theme.color.darygray1};
+          line-height: 3.5rem;
         }
 
         .last-desc {
           width: 100%;
           font: ${({ theme }) => theme.font.body2};
           color: ${({ theme }) => theme.color.gray3};
+          line-height: 2.5rem;
         }
 
         .inquiry {
@@ -96,6 +97,7 @@ const Styled = {
           font: ${({ theme }) => theme.font.body2};
           color: ${({ theme }) => theme.color.gray3};
           text-align: center;
+          line-height: 2.2rem;
         }
       }
       .button-area {
@@ -106,11 +108,10 @@ const Styled = {
         }
       }
     }
-  `
+  `,
 };
 
 const InfoProcedure = () => {
-
   return (
     <Styled.Wrapper>
       <div className="content-wrapper">
@@ -120,10 +121,21 @@ const InfoProcedure = () => {
             <div>
               <h4 className="card__title">STEP 1 비행기 탑승 예약</h4>
               <ul>
-                <li className="card__list">항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다</li>
-                <li className="card__list">봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당 항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고 예약합니다</li>
-                <li className="card__list">항공사에 따라 1인 2~3마리까지 가능합니다</li>
-                <li className="card__list">일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우 기내탑승도 가능합니다</li>
+                <li className="card__list">
+                  항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다
+                </li>
+                <li className="card__list">
+                  봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당
+                  항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고
+                  예약합니다
+                </li>
+                <li className="card__list">
+                  항공사에 따라 1인 2~3마리까지 가능합니다
+                </li>
+                <li className="card__list">
+                  일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우
+                  기내탑승도 가능합니다
+                </li>
               </ul>
             </div>
             <img className="right" src={Step1Img} alt="airport"></img>
@@ -132,10 +144,21 @@ const InfoProcedure = () => {
             <div>
               <h4 className="card__title">STEP 2 비행기 탑승 예약</h4>
               <ul>
-                <li className="card__list">항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다</li>
-                <li className="card__list">봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당 항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고 예약합니다</li>
-                <li className="card__list">항공사에 따라 1인 2~3마리까지 가능합니다</li>
-                <li className="card__list">일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우 기내탑승도 가능합니다</li>
+                <li className="card__list">
+                  항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다
+                </li>
+                <li className="card__list">
+                  봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당
+                  항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고
+                  예약합니다
+                </li>
+                <li className="card__list">
+                  항공사에 따라 1인 2~3마리까지 가능합니다
+                </li>
+                <li className="card__list">
+                  일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우
+                  기내탑승도 가능합니다
+                </li>
               </ul>
             </div>
           </article>
@@ -143,10 +166,21 @@ const InfoProcedure = () => {
             <div>
               <h4 className="card__title">STEP 3 비행기 탑승 예약</h4>
               <ul>
-                <li className="card__list">항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다</li>
-                <li className="card__list">봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당 항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고 예약합니다</li>
-                <li className="card__list">항공사에 따라 1인 2~3마리까지 가능합니다</li>
-                <li className="card__list">일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우 기내탑승도 가능합니다</li>
+                <li className="card__list">
+                  항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다
+                </li>
+                <li className="card__list">
+                  봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당
+                  항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고
+                  예약합니다
+                </li>
+                <li className="card__list">
+                  항공사에 따라 1인 2~3마리까지 가능합니다
+                </li>
+                <li className="card__list">
+                  일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우
+                  기내탑승도 가능합니다
+                </li>
               </ul>
             </div>
             <img className="right" src={Step3Img} alt="airport"></img>
@@ -156,34 +190,54 @@ const InfoProcedure = () => {
             <div>
               <h4 className="card__title">STEP 4 비행기 탑승 예약</h4>
               <ul>
-                <li className="card__list">항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다</li>
-                <li className="card__list">봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당 항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고 예약합니다</li>
-                <li className="card__list">항공사에 따라 1인 2~3마리까지 가능합니다</li>
-                <li className="card__list">일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우 기내탑승도 가능합니다</li>
+                <li className="card__list">
+                  항공기 편당 탑승 가능한 강아지 자리가 한정되어 있습니다
+                </li>
+                <li className="card__list">
+                  봉사자님의 e-ticket과 연락처를 보내주시면 단체에서 해당
+                  항공편에 보낼 수 있는 강아지 자리가 남아있는지 확인하고
+                  예약합니다
+                </li>
+                <li className="card__list">
+                  항공사에 따라 1인 2~3마리까지 가능합니다
+                </li>
+                <li className="card__list">
+                  일반적으로 동물전용칸의 수화물로 보내지만 작은 아이들의 경우
+                  기내탑승도 가능합니다
+                </li>
               </ul>
             </div>
           </article>
           <h3 className="last-title">
-            입국장으로 나가시면 입양자님이 기다리고 계십니다!️<br />
-            유기견이었던 강아지가 평생 가족을 만나기 위해 내딛는 첫 발걸음에 동행해주셔서 감사합니다 :)
+            입국장으로 나가시면 입양자님이 기다리고 계십니다!️
+            <br />
+            유기견이었던 강아지가 평생 가족을 만나기 위해 내딛는 첫 발걸음에
+            동행해주셔서 감사합니다 :)
           </h3>
           <p className="last-desc">
-            입국수속 시 강아지에 대해 묻는다면
-            이 강아지는 입양자가 기다리고 있으며 이동봉사로 데려왔다고 하시면 됩니다.<br />
-            이와 관련해 대부분의 단체에서는 Letter를 준비해 드리며 서류가 준비되어있기 때문에 입국시 전혀 문제될 일이 없습니다!
+            입국수속 시 강아지에 대해 묻는다면 이 강아지는 입양자가 기다리고
+            있으며 이동봉사로 데려왔다고 하시면 됩니다.
+            <br />
+            이와 관련해 대부분의 단체에서는 Letter를 준비해 드리며 서류가
+            준비되어있기 때문에 입국시 전혀 문제될 일이 없습니다!
           </p>
           <div className="inquiry">
-            해당(이동봉사 절차) 정보의 경우 이동봉사를 진행하는 단체, 개인구조자에 따라 약간씩 차이가 있을 수 있습니다. <br />
-            질문이나 자세한 사항은 대상견을 보호하고 있는 단체나 개인 구조자에게 문의 부탁드립니다.
+            해당(이동봉사 절차) 정보의 경우 이동봉사를 진행하는 단체,
+            개인구조자에 따라 약간씩 차이가 있을 수 있습니다. <br />
+            질문이나 자세한 사항은 대상견을 보호하고 있는 단체나 개인 구조자에게
+            문의 부탁드립니다.
           </div>
         </section>
         <div className="button-area">
-          <Button primary rounded padding="1.6rem 7.1rem" font="button">이동 봉사 하러 가기</Button>
-          <Button rounded padding="1.6rem 6.6rem" font="button">대상견 등록하러 가기</Button>
+          <Button primary rounded padding="1.6rem 7.1rem" font="button">
+            이동 봉사 하러 가기
+          </Button>
+          <Button rounded padding="1.6rem 6.6rem" font="button">
+            대상견 등록하러 가기
+          </Button>
         </div>
       </div>
     </Styled.Wrapper>
-    
   );
 };
 
--- src/components/layer/info/InfoWarning.jsx
+++ src/components/layer/info/InfoWarning.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { ReactComponent as WarnIcon } from 'assets/icon/ic_attention-triangle.svg';
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as WarnIcon } from "assets/icon/ic_attention-triangle.svg";
 
 const Styled = {
   Wrapper: styled.div`
@@ -8,7 +8,7 @@ const Styled = {
     margin-bottom: 10rem;
     width: 100vw;
     background-color: rgba(255, 239, 175, 0.17);
-    
+
     .content {
       display: flex;
       flex-direction: column;
@@ -26,19 +26,20 @@ const Styled = {
         font: ${({ theme }) => theme.font.headline};
         color: ${({ theme }) => theme.color.primary};
       }
-      
+
       .description {
         width: 93.6rem;
         font: ${({ theme }) => theme.font.body3};
         color: ${({ theme }) => theme.color.gray3};
         text-align: center;
         word-break: keep-all;
+        line-height: 2.5rem;
         strong {
           color: ${({ theme }) => theme.color.darkgray2};
         }
       }
     }
-  `
+  `,
 };
 
 const InfoWarning = () => {
@@ -48,11 +49,15 @@ const InfoWarning = () => {
         <WarnIcon />
         <h3 className="title">이동봉사 진행 전 주의하세요!</h3>
         <p className="description">
-          최근 정상적으로 서류절차를 거치지 않고 &apos;본인의 반려견으로 등록하여 강아지를 데려가 달라&apos;는 식의 거짓말을 필요로 하는 이동봉사자를 모집하는 경우가 있습니다.
-          이러한 내용의 이동봉사를 진행 할 시 입국 심사 과정에서 봉사자에게 불이익이 생길 수도 있습니다.
-          특히,
-          <strong>단체나 개인구조자가 아닌 개인이 찾는 대가성 이동봉사에 주의 하시고
-            봉사자님께 불이익이 가지 않도록 Permit과 Entry Number, 검역 확인증, 이동봉사자임을 확인하는 레터 등의 서류를 확인 하시기 바랍니다.</strong>
+          최근 정상적으로 서류절차를 거치지 않고 &apos;본인의 반려견으로
+          등록하여 강아지를 데려가 달라&apos;는 식의 거짓말을 필요로 하는
+          이동봉사자를 모집하는 경우가 있습니다. 이러한 내용의 이동봉사를 진행
+          할 시 입국 심사 과정에서 봉사자에게 불이익이 생길 수도 있습니다. 특히,
+          <strong>
+            단체나 개인구조자가 아닌 개인이 찾는 대가성 이동봉사에 주의 하시고
+            봉사자님께 불이익이 가지 않도록 Permit과 Entry Number, 검역 확인증,
+            이동봉사자임을 확인하는 레터 등의 서류를 확인 하시기 바랍니다.
+          </strong>
         </p>
       </section>
     </Styled.Wrapper>
