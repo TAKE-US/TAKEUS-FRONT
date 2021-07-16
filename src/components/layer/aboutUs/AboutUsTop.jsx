c950bc0e6ec51e343d0d86554d445bd30cfb341f
leeinsong <insong1216@gmail.com>
2021-07-16 17:25:56 +0900

    style: font 수정

--- src/components/layer/aboutUs/AboutUsTop.jsx
+++ src/components/layer/aboutUs/AboutUsTop.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import styled from "styled-components";
-import backgroundAboutusImg from "assets/img/img_AboutUsTop.png";
+import React from 'react';
+import styled from 'styled-components';
+import backgroundAboutusImg from 'assets/img/img_AboutUsTop.png';
 
 const Styled = {
   Wrapper: styled.div`
@@ -41,7 +41,7 @@ const Styled = {
       justify-content: space-between;
       text-align: center;
       line-height: 2.6rem;
-      font: ${({ theme }) => theme.font.subheading};
+      font: ${({ theme }) => theme.font.body2};
       color: ${({ theme }) => theme.color.white};
       p:nth-child(2) {
         padding-top: 2.8rem;
@@ -54,23 +54,12 @@ const AboutUsTop = () => {
   return (
     <Styled.Wrapper>
       <div className="title">TAKE US, TAKERS!</div>
-      <div className="subtitle">
-        유기견들의 새로운 삶을 향한 첫 발자국을 함께합니다.
-      </div>
+      <div className="subtitle">유기견들의 새로운 삶을 향한 첫 발자국을 함께합니다.</div>
       <div className="contour"></div>
       <div className="content">
-        <p>
-          TAKE US는 유기견 해외이동봉사를 보다 널리 알리자는 목적과 함께 탄생한
-          웹 서비스입니다.
-        </p>
-        <p>
-          다양한 카페와 블로그, SNS 등 여러 채널에 대상견의 정보가 산개되어있는
-          탓에 이동봉사를 하고자 하는
-        </p>
-        <p>
-          봉사자가 대상견의 정보를 탐색하는 것에 어려움을 겪는다는 것에서부터
-          착안하여 탄생하였습니다.
-        </p>
+        <p>TAKE US는 유기견 해외이동봉사를 보다 널리 알리자는 목적과 함께 탄생한 웹 서비스입니다.</p>
+        <p>다양한 카페와 블로그, SNS 등 여러 채널에 대상견의 정보가 산개되어있는 탓에 이동봉사를 하고자 하는</p>
+        <p>봉사자가 대상견의 정보를 탐색하는 것에 어려움을 겪는다는 것에서부터 착안하여 탄생하였습니다.</p>
       </div>
     </Styled.Wrapper>
   );
