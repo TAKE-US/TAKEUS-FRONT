f3345b551e17bae94080211dc3988466d77b6e17
홍준엽(Ethan) <hjy@gabia.com>
2021-08-03 09:10:18 +0900

    feat: meta tag 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import {hydrate, render} from "react-dom";
+import { hydrate, render } from "react-dom";
 import App from "./App";
+import store from "redux/store";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
-import { Provider } from "react-redux";
-import store from "redux/store";
-
 
 const rootElement = document.getElementById("root");
 
 if (rootElement.hasChildNodes()) {
-  hydrate(
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>, rootElement);
+  hydrate(<App />, rootElement);
 } else {
   render(
     <Provider store={store}>
