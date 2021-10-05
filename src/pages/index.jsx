adf1d6f2cf9b66d3ad12580b987f4c0794854746
ingong <insong1216@gmail.com>
2021-10-05 21:14:45 +0900

    feat: Redirect Router 추가 및 해당 Page 추가

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,3 +8,4 @@ export { default as ReviewPage } from "./review/ReviewPage";
 export { default as ReviewPostPage } from "./review/ReviewPostPage";
 export { default as MainPage } from "./MainPage";
 export { default as MyPage } from "./my/MyPage";
+export { default as KakaoPage } from "./oAuth/KakaoPage";
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import DogRouter from './DogRouter';
-import ReviewRouter from './ReviewRouter';
+import DogRouter from "./DogRouter";
+import ReviewRouter from "./ReviewRouter";
 
 import {
   LoginPage,
@@ -9,20 +9,21 @@ import {
   AboutUsPage,
   MainPage,
   MyPage,
+  KakaoPage,
 } from "pages";
 
 const Router = () => {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={ MainPage } />
-        <Route path="/login" component={ LoginPage } />
-        <Route path="/info" component={ InfoPage } />
-        <Route path="/about" component={ AboutUsPage } />
-        <Route path="/mypage" component={ MyPage } />
-        
-        <Route path="/dog" component={ DogRouter } />
-        <Route path="/review" component={ ReviewRouter } />
+        <Route exact path="/" component={MainPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/info" component={InfoPage} />
+        <Route path="/about" component={AboutUsPage} />
+        <Route path="/mypage" component={MyPage} />
+        <Route path="/dog" component={DogRouter} />
+        <Route path="/review" component={ReviewRouter} />
+        <Route path="/oauth/callback/kakao" component={KakaoPage}></Route>
       </Switch>
     </>
   );
