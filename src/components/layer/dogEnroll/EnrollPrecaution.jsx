b12efea6f485389cded3d396db7d2bcff45ec21e
moonhopark <74300832+moonhopark@users.noreply.github.com>
2021-11-02 20:19:43 +0900

    feat: 대상견 등록 전 주의사항 페이지 (#141)

--- src/components/layer/dogEnroll/EnrollPrecaution.jsx
+++ src/components/layer/dogEnroll/EnrollPrecaution.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const EnrollInfoWrap = styled.div`
-  padding: 0 12rem;
   header {
     text-align: center;
     padding-bottom: 4.8rem;
@@ -44,9 +43,13 @@ const EnrollInfo = () => {
         <article>
           <ul>
             <li>대상견의 기본적인 정보와 함께 정확한 출국 지역(공항) 정보를 입력해주세요</li>
-            <li>대상견의 건강 상태와 중성화, 접종 여부 등의 의료처치 여부에 대해 자세히 작성해주세요.</li>
+            <li>
+              대상견의 건강 상태와 중성화, 접종 여부 등의 의료처치 여부에 대해 자세히 작성해주세요.
+            </li>
             <li>봉사자분들이 알아야 할 주의 사항, 필요한 정보 등에 대해 작성해주세요</li>
-            <li>영주권자, 시민권자이거나 특정 비자가 필요한 경우 해당 내용에 대해서 작성해 주세요.</li>
+            <li>
+              영주권자, 시민권자이거나 특정 비자가 필요한 경우 해당 내용에 대해서 작성해 주세요.
+            </li>
             <li>국가별로 특정 조건(항공사등)이 필요한 경우 이에 대해 작성해 주세요</li>
           </ul>
         </article>
--- src/router/DogRouter.jsx
+++ src/router/DogRouter.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { DogPage, DogDetailPage, DogEnrollPage } from "pages";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { DogPage, DogDetailPage, DogEnrollPage, DogEnrollCautionPage } from 'pages';
 
 const DogRouter = ({ match }) => {
   const path = match.path;
   return (
     <>
-      <Route exact path={path + "/search"} component={DogPage} />
-      <Route path={path + "/search/airport"} component={DogPage} />
-      <Route exact path={path + "/search/:id"} component={DogDetailPage} />
-      <Route path={path + "/search/:id/edit"}>
+      <Route exact path={path + '/search'} component={DogPage} />
+      <Route path={path + '/search/airport'} component={DogPage} />
+      <Route exact path={path + '/search/:id'} component={DogDetailPage} />
+      <Route path={path + '/search/:id/edit'}>
         <DogEnrollPage edit={true} />
       </Route>
-      <Route path={path + "/enroll"} component={DogEnrollPage} />
+      <Route path={path + '/enroll'} component={DogEnrollPage} />
+      <Route path={path + '/enrollcaution'} component={DogEnrollCautionPage} />
     </>
   );
 };
