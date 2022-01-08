4da0ab4783b322d5e24569b901b2b0ca20afdbf9
chaerin <achrvv@sookmyung.ac.kr>
2022-01-08 15:37:28 +0900

    fix: fix review post & put

--- src/components/atoms/ReviewCard.jsx
+++ src/components/atoms/ReviewCard.jsx
@@ -88,9 +88,8 @@ const ReviewCard = ({ review, editHandler, deleteHandler }) => {
         </article>
         <section className="description">
           <p>
-            작성일ㅣ{formatDate(review.writeDate)} &nbsp;&nbsp;지역ㅣ{review.endingAirport.split(" ")[0]}{" "}
-            &nbsp;&nbsp;봉사단체ㅣ
-            {review.institutionName}
+            작성일ㅣ{formatDate(review.writeDate)} &nbsp;&nbsp;지역ㅣ{review.endingAirport.split(" ")[0]} &nbsp;&nbsp;
+            {review.isInstitution}ㅣ{review.institutionName}
           </p>
           {isLogin && (
             <div className="button-wrap">
