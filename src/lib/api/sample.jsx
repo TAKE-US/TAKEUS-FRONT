1683f6508654f8cd91b8e65334063a6150283564
CHAERIN AN <72637095+chaerin00@users.noreply.github.com>
2022-01-25 23:25:33 +0900

    feat: feat env setting (#163)
    * feat: feat env setting
    
    * fix: fix run
    
    * fix: fix echo
    
    * fix: fix cat env
    
    * fix: fix append env
    
    * fix: fix cd env

--- src/lib/api/sample.jsx
+++ src/lib/api/sample.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? '/' : process.env.API_DOMAIN,
+  baseURL: process.env.NODE_ENV === 'development' ? '/' : process.env.REACT_APP_API_DOMAIN,
 });
 
 export const getDogs = async () => {
