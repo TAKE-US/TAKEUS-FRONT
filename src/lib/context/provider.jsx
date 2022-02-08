d449d9fe84f440a71de61d3c513655a8d569eed2
ingong <insong1216@gmail.com>
2022-02-08 14:27:07 +0900

    refactor: Provider 모듈에 토큰 유효성 검증 로직 이동

--- src/lib/context/provider.jsx
+++ src/lib/context/provider.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import { LoginStateContext, LoginDispatchContext } from './context';
 
+const isValid = () => {
+  const getIssuedAtTime = () => localStorage.getItem('issuedAt');
+  const getNowTime = () => Math.floor(+new Date() / 1000);
+  const ONE_HOUR = 3600;
+
+  return getNowTime() - +getIssuedAtTime() < ONE_HOUR;
+};
+
 const LoginProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
+  const isValidToken = isValid();
+  const loginState = isLogin & isValidToken;
 
   return (
-    <LoginStateContext.Provider value={isLogin}>
+    <LoginStateContext.Provider value={loginState}>
       <LoginDispatchContext.Provider value={setIsLogin}> {children}</LoginDispatchContext.Provider>
     </LoginStateContext.Provider>
   );
