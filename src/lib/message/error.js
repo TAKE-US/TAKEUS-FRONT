15b6259e1116637201f219d539b03b80d5dda360
ingong <insong1216@gmail.com>
2022-02-25 17:55:52 +0900

    feat: ERROR MESSAGE 추가

--- src/lib/message/error.js
+++ src/lib/message/error.js
@@ -2,6 +2,7 @@ const ERROR_MESSAGE = {
   NO_COUNTRY_SELECTED: '국가명을 선택해주세요',
   NO_AIRPORT_SELECTED: '공항명을 선택해주세요',
   NO_INSTITUTION_SELECTED: '단체명을 입력해주세요',
+  REMOVE_INSTITUTION_NAME: '단체명을 지워주세요',
   NO_CONTACT_SELECTED: '연락처를 최소 1개 이상 입력해주세요',
 };
 
